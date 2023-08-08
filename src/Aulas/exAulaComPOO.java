26903ccbe99da935046ad8c2d4bc9c3d85d1b119
Victor "Noobzin <67431024+Noobz1n@users.noreply.github.com>
2023-08-08 11:55:52 -0300

    atualizacao ex

--- src/Aulas/exAulaComPOO.java
+++ src/Aulas/exAulaComPOO.java
@@ -24,11 +24,10 @@ public class exAulaComPOO {
         y.c = sc.nextDouble();
 
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-        p = (y.a + y.b + y.c) / 2.0;
+        double areaX = x.area();
 
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        double areaY = y.area();
+
         System.out.printf("Triangle X area: %.4f%n", areaX);
         System.out.printf("Triangle Y area: %.4f%n", areaY);
          
---
+++ src/Entities/Product.java
@@ -0,0 +1,28 @@
+package Entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+        this.quantity += quantity; //o this significa que vai acessar o atributo da classe, nao o quantity do parametro.
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+
+    public String toString(){
+        return name
+                + ", "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total $: "
+                + String.format("%.2f", totalValueInStock());
+    }
+}

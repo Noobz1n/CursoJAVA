1959a07a2c9fcffc90bdfc30d56300ffef03881f
Victor "Noobzin <67431024+Noobz1n@users.noreply.github.com>
2023-08-17 11:15:45 -0300

    aprendendo a gerar construtores e get e setters automaticamente

--- src/Entities/Encapsulation_product.java
+++ src/Entities/Encapsulation_product.java
@@ -4,30 +4,38 @@ public class Encapsulation_product {
     private String name;
     private double price;
     private int quantity;
-    public Encapsulation_product() {
+
+    public Encapsulation_product(){
+
     }
     public Encapsulation_product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
+
     public Encapsulation_product(String name, double price) {
         this.name = name;
         this.price = price;
     }
+
+    public Encapsulation_product(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
-    }
-    public int getQuantity() {
-        return quantity;
     }
 }

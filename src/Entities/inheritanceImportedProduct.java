64cf74c2cfe4b7c3e8f560d0509b085b67120551
Victor <vitorusko@gmail.com>
2024-02-11 21:49:53 -0300

    Exercicio de herança

---
+++ src/Entities/inheritanceImportedProduct.java
@@ -0,0 +1,36 @@
+package Entities;
+
+import java.time.LocalDate;
+
+public class inheritanceImportedProduct extends inheritanceProduct {
+	
+	private Double customsFee;
+
+	public inheritanceImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	public Double totalPrice() {
+		return getPrice() + customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return getName() 
+				+ " $ " 
+				+ String.format("%.2f", totalPrice())
+				+ " (Customs fee: $ " 
+				+ String.format("%.2f", customsFee)
+				+ ")";
+	}
+	
+}
---
+++ src/Exercicios/products.java
@@ -0,0 +1,61 @@
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entities.inheritanceImportedProduct;
+import Entities.inheritanceProduct;
+import Entities.inheritanceUsedProduct;
+
+
+public class products {
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<inheritanceProduct> list = new ArrayList<>();
+		
+		System.out.print("Entre com o numero de produtos: ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Datos do produto #" + i + ":");
+			System.out.print("Comum, usado, importado (c/u/i)? ");
+			char type = sc.next().charAt(0);
+			System.out.print("Nome: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Preco: ");
+			double price = sc.nextDouble();
+			if (type == 'c') {
+				list.add(new inheritanceProduct(name, price));
+			}
+			else if (type == 'u') {
+				System.out.print("Data de fabricacao (DD/MM/YYYY): ");
+				LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+				list.add(new inheritanceUsedProduct(name, price, date));
+			}
+			else {
+				System.out.print("Taxa de importacao: ");
+				double customsFee = sc.nextDouble();
+				list.add(new inheritanceImportedProduct(name, price, customsFee));
+			}
+		}
+
+		System.out.println();
+		System.out.println("ETIQUETAS DE PRECO:");
+		for (inheritanceProduct prod : list) {
+			System.out.println(prod.priceTag());
+		}
+		
+		sc.close();		
+		
+	
+		
+	}
+
+}
